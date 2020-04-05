938ab4aca61b4ff8c42e45d02c604ee011f892a9
andersonms1 <andersoonn.ams@gmail.com>
2020-04-05 14:56:41 -0300

    format

---
+++ song-list/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import reducers from "./reducers";
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
